d0d02d30cfd6d9e5fbefebf3cac01882e329f895
Guilherme Argilar <guilherme.argilar@gmail.com>
2023-10-11 17:43:38 -0300

    Adicionado JPA para Orcamentos, criado o EndPoint de salvar o Orcamentos e listar por data

--- src/main/java/com/projarq/dominio/servicos/ServicosOrcamento.java
+++ src/main/java/com/projarq/dominio/servicos/ServicosOrcamento.java
@@ -1,32 +1,56 @@
 package com.projarq.dominio.servicos;
 
+import java.time.LocalDate;
+import java.util.stream.Collectors;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.projarq.dominio.entidades.Cidade;
 import com.projarq.dominio.entidades.Orcamento;
-import com.projarq.persistencia.OrcamentoRepository;
+import com.projarq.dominio.repositorios.RepOracamento;
 
 @Service
 public class ServicosOrcamento {
-    private final OrcamentoRepository orcamentoRepository;
+    private final RepOracamento orcamentoRepository;
     private final ServicosCustoAdicional servicosCustoAdicional;
     private final ServicoCustoBasico servicoCustoBasico;
     private final ServicosCidade servicosCidade;
-    public ServicosOrcamento(OrcamentoRepository orcamentoRepository, ServicosCidade servicosCidade,ServicosCustoAdicional servicosCustoAdicional, ServicoCustoBasico servicoCustoBasico) {
+    private final ServicoImposto servicoImposto;
+
+    public ServicosOrcamento(RepOracamento orcamentoRepository,
+            ServicosCidade servicosCidade,
+            ServicosCustoAdicional servicosCustoAdicional,
+            ServicoCustoBasico servicoCustoBasico,
+            ServicoImposto servicoImposto) {
         this.orcamentoRepository = orcamentoRepository;
+        this.servicosCidade = servicosCidade;
         this.servicosCustoAdicional = servicosCustoAdicional;
         this.servicoCustoBasico = servicoCustoBasico;
-        this.servicosCidade = servicosCidade;
+        this.servicoImposto = servicoImposto;
     }
 
     public Cidade obterCidadePorId(Long id) {
         return servicosCidade.obterCidadePorId(id);
     }
 
-    public double calcularPrecoDeNovaEncomenda(Cidade cidadeOrigem, Cidade cidadeDestino, int gramas ) {
+    public double calcularPrecoDeNovaEncomenda(Cidade cidadeOrigem, Cidade cidadeDestino, int gramas) {
+        Long id = 1L;
         double custoBasico = servicoCustoBasico.getCustoBasico(cidadeOrigem, cidadeDestino, gramas);
-        double precoAdicional = servicosCustoAdicional.getAdicionalPorPeso(gramas); 
-        double precoTotal = custoBasico + precoAdicional;
+        double precoAdicional = servicosCustoAdicional.getAdicionalPorPeso(gramas);
+        double imposto = servicoImposto.getImposto(custoBasico);
+        double precoTotal = custoBasico + precoAdicional + imposto;
+        LocalDate data = LocalDate.now();
+        Orcamento orcamento = new Orcamento(id, cidadeOrigem, cidadeDestino, gramas, precoTotal, imposto, 0.00, data);
+        orcamentoRepository.salvar(orcamento);
         return precoTotal;
     }
+
+    public List<Orcamento> listarOrcamentosPorData(LocalDate data) {
+        return orcamentoRepository
+            .findAll()
+            .stream()
+            .filter(orcamento -> orcamento.getDataOrcamento().isEqual(data))
+            .collect(Collectors.toList());
+    }
+    
 }
