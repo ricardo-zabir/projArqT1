9eee4b3947d8976853541cbc724e208b78b005da
Guilherme Argilar <guilherme.argilar@gmail.com>
2023-11-14 14:38:51 -0300

    configurado o naming server, api gateway e ajustado o pom do monolito

---
+++ api-gateway/src/main/java/com/engsoft2/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,25 @@
+package com.engsoft2.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+    @Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.path("/cidadesatendidas/**")
+						.uri("lb://monolito"))
+				.route(p -> p.path("/retornaPromoVigente/**")
+						.uri("lb://monolito"))
+				.route(p -> p.path("/validacep/**")
+						.uri("lb://monolito"))
+				.route(p -> p.path("/calculafrete/**")
+						.uri("lb://monolito"))
+				.route(p -> p.path("/listaorcamentospordata/**")
+						.uri("lb://monolito"))
+				.build();
+	}    
+}
